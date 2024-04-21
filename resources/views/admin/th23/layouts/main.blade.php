6adb8c98b62595c96fa1c035b8edcab1556e2b01
cok abimanyu <webbrilian.com>
2024-04-21 12:19:02 +0800

    update icon title simantu

--- resources/views/admin/th23/layouts/main.blade.php
+++ resources/views/admin/th23/layouts/main.blade.php
@@ -7,6 +7,7 @@
   <title>{{$title}}</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+  <link rel="icon" href="{{ asset('img/simantu.ico')}}"/>
   <!-- Bootstrap 3.3.7 -->
   <!-- <link rel="stylesheet" href="../assets/bower_components/bootstrap/dist/css/bootstrap.min.css"> -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
